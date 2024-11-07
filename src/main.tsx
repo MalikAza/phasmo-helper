0f2cd396fecf0469d66e6a089e6a6fbfa869612a
Dakota <dakota742009@gmail.com>
2024-11-07 15:47:49 +0100

    fix: added StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from '@app/app'
 import '@assets/styles/index.css'
+import { StrictMode } from 'react'
 
 const rootElement = document.getElementById('root')
 
 createRoot(rootElement!)
-  .render(<App />)
+.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
