9deb8b3d90dd79aff2f8e61acb3557a52025a8d9
Dakota <dakota742009@gmail.com>
2025-01-26 00:24:48 +0100

    close #3

--- src/components/ghost.btn.tsx
+++ src/components/ghost.btn.tsx
@@ -2,7 +2,7 @@ import { isGhostPossible, URLEncodeGhostName } from "@/features/ghosts/utils.gho
 import useJournal from "@/hooks/journal.hook"
 import { cn } from "@/lib/utils"
 import { Ghost } from "@/types/ghosts.types"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 type Props = {
   ghost: Ghost
@@ -10,15 +10,24 @@ type Props = {
 
 function GhostBtn({ ghost }: Props) {
   const { state } = useJournal()
+  const location = useLocation()
 
+  const endpoint = location.pathname.split('/').pop()
   const encodedGhostName = URLEncodeGhostName(ghost.id)
+  const isSelected = endpoint === encodedGhostName
+
+  console.log(endpoint, encodedGhostName, isSelected)
 
   return (
     <Link
       to={`/${encodedGhostName}`}
-      className={cn(isGhostPossible(ghost, state.evidences) ? 'opacity-100' : 'opacity-10')}
+      className={cn(
+        isGhostPossible(ghost, state.evidences) ? 'opacity-100' : 'opacity-10',
+        'relative'
+      )}
     >
-      {ghost.name}
+      <span>{ghost.name}</span>
+      {isSelected && <span className="absolute h-[3.4rem] w-[9rem] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-black rounded-[75%]" />}
     </Link>
   )
 }
