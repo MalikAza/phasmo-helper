bad8ec5bb534dfe50d13a7293d096b036a5b929e
Dakota <dakota742009@gmail.com>
2025-10-23 21:45:27 +0200

    fix: moroi details

--- src/components/ghosts/moroi.tsx
+++ src/components/ghosts/moroi.tsx
@@ -11,18 +11,18 @@ function Moroi() {
   ]
   const speeds = [
     '1,5 m/s (base avec santé >50%)',
-    '2,25 m/s (base avec santé <5%',
+    '2,25 m/s (base avec santé <5%)',
     '3,71 m/s (accélération maximum)',
     '+0,08 m/s touts les 5% de santé perdu en dessous de 45%'
   ]
   const details = [
-    "Maudit un joueur lors d'une réponse à la spirit box ou micro parabolique",
+    "Maudit un joueur lors d'une réponse à la spirit box, micro parabolique ou enregistreur sonore",
     "Être maudit faire perdre 2 fois plus de santé",
     "Être maudit fait perdre de la santé à la lumière",
     "Malédiction uniquement dans la maison",
     "La malédiction se soigne avec une pilule",
     "La malédiction se soigne en mourrant puis en étant resssusciter",
-    "L'encens le désoriente pendant 7,5s (au lieu de 5s) durant une chasse",
+    "L'encens le désoriente pendant 7s (au lieu de 5s) durant une chasse",
 
   ]
 
